e6a5bb0f6d53039f8a8408512c857b45c3f33560
xiitorii <srgjdrvn@gmail.com>
2026-04-14 11:21:12 +0300

    init

---
+++ src/test/java/ru/xiitori/financemanager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.xiitori.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
