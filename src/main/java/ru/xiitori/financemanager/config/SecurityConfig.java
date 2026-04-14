eba06f304d0709a3aa927ca3d1456d4579972f69
xiitorii <srgjdrvn@gmail.com>
2026-04-14 15:27:46 +0300

    feat: cofigure spring security

---
+++ src/main/java/ru/xiitori/financemanager/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package ru.xiitori.financemanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain
+            (HttpSecurity http,
+            UserDetailsService userDetailsService) {
+        http
+                .cors(AbstractHttpConfigurer::disable) // временно
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(userDetailsService);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder password() {
+        return new BCryptPasswordEncoder();
+    }
+}
