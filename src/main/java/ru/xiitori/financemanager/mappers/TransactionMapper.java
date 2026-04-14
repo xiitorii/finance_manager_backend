49a917fd3a7d04d64be720c987d4be28986aac09
xiitorii <srgjdrvn@gmail.com>
2026-04-14 16:02:37 +0300

    feat: make TransactionMapper first method with mapstruct

---
+++ src/main/java/ru/xiitori/financemanager/mappers/TransactionMapper.java
@@ -0,0 +1,15 @@
+package ru.xiitori.financemanager.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.xiitori.financemanager.model.dto.TransactionResponseDTO;
+import ru.xiitori.financemanager.model.entity.Transaction;
+
+@Mapper(componentModel = "spring")
+public interface TransactionMapper {
+
+    @Mapping(target = "userId", source="user.id")
+    TransactionResponseDTO mapToDto(Transaction transaction);
+//
+//    Transaction mapToEntity(TransactionRequestDTO dto);
+}
